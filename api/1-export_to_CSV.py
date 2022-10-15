e2964c8df6a9c5b6eebf55898717bd97bff8eafa
izzeddin62 <i.ishimwe@alustudent.com>
2022-10-15 16:01:07 +0200

    add quotes to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """get TODO list"""
 
-import json
 import csv
+import json
 import requests
 import sys
 if __name__ == "__main__":
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
     res = requests.get(link)
     todos = json.loads(res.text)
-    csv_data = [[i["userId"],
+    csv_data = [["{}".format(i["userId"]),
                 user["username"], i["completed"], i["title"]] for i in todos]
     with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
-        writer = csv.writer(f)
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
         writer.writerows(csv_data)
